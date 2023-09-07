a7efcd959797c497fbbac1872a55ee2464ac8483
quentin damois <quentin.damois@efrei.net>
2023-09-07 09:05:54 -0400

    commentary added to Security class.

--- Assignment2/src/GeneralClass/Security.java
+++ Assignment2/src/GeneralClass/Security.java
@@ -3,6 +3,14 @@ package GeneralClass;
 import javax.swing.*;
 
 public class Security {
+    /**
+     * This is the scercurity class. The class stored static method used to handle exeption.
+     * */
+    /**
+     * This is the safeIntInput method. It prevents NumberFormatExeption to appear when inputing an int.
+     * @param question : this string is displayed in the screen when the user is asked to input an int.
+     * @return res : the int inputed by the user.
+     * */
     public static int safeIntInput(String question) {
         int res;
         try {
