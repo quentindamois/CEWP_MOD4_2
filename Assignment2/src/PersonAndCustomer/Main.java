7bc7d90c4a0e864ce61cf32e64d7bf05b70f427f
quentin damois <quentin.damois@efrei.net>
2023-09-06 21:38:53 -0400

    Updated the class main in the package PersonAndCustomer.

--- Assignment2/src/PersonAndCustomer/Main.java
+++ Assignment2/src/PersonAndCustomer/Main.java
@@ -1,46 +1,17 @@
 package PersonAndCustomer;
 import javax.swing.JOptionPane;
+import GeneralClass.Security;
 public class Main {
     public static void main(String[] arg) {
         String name = JOptionPane.showInputDialog("Enter the customer name :");
-        if (name != null) {
-            String address = JOptionPane.showInputDialog("Enter enter the customer address :");
-            if (address != null) {
-                String telephone = JOptionPane.showInputDialog("Enter the customer's phone number :");
-                if (telephone != null) {
-                    String tempoString = JOptionPane.showInputDialog("Enter the customer number: ");
-                    System.out.println(tempoString);
-                    if (tempoString != null) {
-                        int numberCustomer = Integer.parseInt(tempoString);
-                        tempoString = JOptionPane.showInputDialog("Enter if the customer is on the mailing list: ");
-                        if (tempoString != null) {
-                            boolean mail = Boolean.parseBoolean(tempoString);
-                            Customer customer1 = new Customer(numberCustomer, mail, name, telephone, address);
-                            JOptionPane.showMessageDialog(null, customer1);
-                            tempoString = JOptionPane.showInputDialog("Enter the total amount of dollars spend: ");
-                            if(tempoString != null) {
-                                PreferredCustomer customer2 = new PreferredCustomer(Integer.parseInt(tempoString),numberCustomer, mail, name, telephone, address);
-                                JOptionPane.showMessageDialog(null, customer2);
-                            }
-                            else {
-                                JOptionPane.showMessageDialog(null, "Wrong input, program is ending");
-                            }
-                        }
-                        else {
-                            JOptionPane.showMessageDialog(null, "Wrong input, program is ending");
-                        }
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Wrong input, program is ending");
-                    }
-                } else {
-                    JOptionPane.showMessageDialog(null, "Wrong input, program is ending");
-                }
-            } else {
-                JOptionPane.showMessageDialog(null, "Wrong input, program is ending");
-            }
-        } else {
-            JOptionPane.showMessageDialog(null, "Wrong input, program is ending");
-        }
-
+        String address = JOptionPane.showInputDialog("Enter enter the customer address :");
+        String telephone = JOptionPane.showInputDialog("Enter the customer's phone number :");
+        int numberCustomer = Security.safeIntInput("Enter the customer number: ");
+        boolean mail = Boolean.parseBoolean(JOptionPane.showInputDialog("Enter true if the customer is on the mailing list if not enter something else :"));
+        Customer customer1 = new Customer(numberCustomer, mail, name, telephone, address);
+        JOptionPane.showMessageDialog(null, customer1);
+        PreferredCustomer customer2 = new PreferredCustomer(Security.safeIntInput("Enter the total amount of dollars spend: "),numberCustomer, mail, name, telephone, address);
+        JOptionPane.showMessageDialog(null, customer2);
+        System.exit(0);
     }
 }
