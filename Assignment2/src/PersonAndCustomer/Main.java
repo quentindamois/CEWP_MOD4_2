85675de4773b253ef3803535cd3ba0290dac3514
quentin damois <quentin.damois@efrei.net>
2023-09-09 13:25:31 -0400

    main() in PersonAndCustomer imporved by remplacing unsufae input by safer input with Security class.

--- Assignment2/src/PersonAndCustomer/Main.java
+++ Assignment2/src/PersonAndCustomer/Main.java
@@ -1,13 +1,15 @@
 package PersonAndCustomer;
 import javax.swing.JOptionPane;
 import GeneralClass.Security;
+import GeneralClass.wrongCharInString;
+
 public class Main {
     public static void main(String[] arg) {
-        String name = JOptionPane.showInputDialog("Enter the customer name :");
-        String address = JOptionPane.showInputDialog("Enter enter the customer address :");
-        String telephone = JOptionPane.showInputDialog("Enter the customer's phone number :");
+        String name = Security.safeNameInput("Enter the customer name :");
+        String address = Security.safeNameInput("Enter enter the customer address :");
+        String telephone = Security.safePhoneNumberInput("Enter the customer's phone number :");
         int numberCustomer = Security.safeIntInput("Enter the customer number: ");
-        boolean mail = Boolean.parseBoolean(JOptionPane.showInputDialog("Enter true if the customer is on the mailing list if not enter something else :"));
+        boolean mail = Security.safeBooleanInput("Enter true if the customer is on the mailing list if no the enter false.");
         Customer customer1 = new Customer(numberCustomer, mail, name, telephone, address);
         JOptionPane.showMessageDialog(null, customer1);
         PreferredCustomer customer2 = new PreferredCustomer(Security.safeIntInput("Enter the total amount of dollars spend: "),numberCustomer, mail, name, telephone, address);
