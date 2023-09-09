d51204e0ab650c4cc1c2780933567484a5dfc481
quentin damois <quentin.damois@efrei.net>
2023-09-08 21:37:17 -0400

    comment added to the RollingDice.java

--- Assignment2/src/GUIRollingDice/RollingDice.java
+++ Assignment2/src/GUIRollingDice/RollingDice.java
@@ -6,12 +6,36 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class RollingDice extends JFrame {
+    /**
+     * This is the RollingDice class. This classis an extgension of the JFrame class.
+     * */
+    /**
+     * panel is a field on which store the label and the button.
+     * */
     private JPanel panel;
+    /**
+     * dice1 is a JLabel variable that store a dice picture.
+     * */
     private JLabel dice1;
+    /**
+     * dice2 is a JLabel variable that store a dice picture
+     * */
     private JLabel dice2;
+    /**
+     * submit is a JButton that store the button rolling the dice.
+     * */
     private JButton submit;
+    /**
+     * WINDOW_WIDTH is an int constant usede to define the window width.
+     * */
     private final int WINDOW_WIDTH = 700;
+    /**
+     * WINDOW_HEIGHT is an int constant used to define the window height.
+     * */
     private final int WINDOW_HEIGHT = 350;
+    /**
+     * This is the constructor of the class RollingDice that is used to start the method that roll dice.
+     * */
     public RollingDice() {
         setTitle("Rolling Dice Simulator");
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -20,6 +44,9 @@ public class RollingDice extends JFrame {
         add(panel);
         setVisible(true);
     }
+    /**
+     * The addContent create the JPanel. It also creates and add to the JPanel the two JLabel wtoring the dice picture and the JButton.
+     * */
     public void addContent() {
         this.panel = new JPanel();
         ImageIcon dice1Content = new ImageIcon(".\\Assignment2\\src\\GUIRollingDice\\dice" + (int)(Math.random() * 5) + ".png");
@@ -35,6 +62,14 @@ public class RollingDice extends JFrame {
         this.panel.add(submit);
     }
     private class rolling implements ActionListener {
+        /**
+         * This is the rolling class. Its purpose is to implement the ActionListener interface and to create the metode to roll the dice.
+         * */
+        /**
+         * ActionPerformed is a method that is executed when the roll button is click on. The methode generate randomly 2 int between 0 and 5.
+         * Each int is added to a string, togeter they form a path to one of six drawing of dice.
+         * The file path are then used to load new dice prictures.
+         * */
         public void actionPerformed(ActionEvent e) {
             ImageIcon dice1Content = new ImageIcon(".\\Assignment2\\src\\GUIRollingDice\\dice" + (int)(Math.random() * 5) + ".png");
             ImageIcon dice2Content = new ImageIcon(".\\Assignment2\\src\\GUIRollingDice\\dice" + (int)(Math.random() * 5) + ".png");
@@ -42,6 +77,9 @@ public class RollingDice extends JFrame {
             dice2.setIcon(dice2Content);
         }
     }
+    /**
+     * This is the main method. It is the first methode executed.
+     * */
     public static void main(String[] arg) {
         new RollingDice();
     }
